b35214e02871c955b8059ad34f9c3208d648caba
Alex <oleksandrilnytskyi@gmail.com>
2021-08-15 22:56:50 +0300

    test

--- main.js
+++ main.js
@@ -11,11 +11,15 @@ function counts() {
 
     if (days < 10) {
         days = '0' + days
-    } else if (hours < 10) {
+    }
+
+    if (hours < 10) {
         hours = '0' + hours
-    } else if (minutes < 10) {
+    }
+
+    if (minutes < 10) {
         minutes = '0' + minutes
-    } 
+    }
 
     if (seconds < 10) {
         seconds = '0' + seconds
